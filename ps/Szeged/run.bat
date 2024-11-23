aa20d9a0110c6b69cc52cd4424acda5164cecc63
Téringer Gergő <71704189+Tekergo2004@users.noreply.github.com>
2024-11-23 18:27:24 +0100

    Frissítés a tökéletes futáshoz

--- ps/Szeged/run.bat
+++ ps/Szeged/run.bat
@@ -1,2 +1,4 @@
+cd c:
+cd C:\Szeged
 Powershell.exe -executionpolicy remotesigned -File ".\ou-group-creation-szeged.ps1"
 Powershell.exe -executionpolicy remotesigned -File ".\users-group-add-szeged.ps1"
