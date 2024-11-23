24542a4fac202850218a179fd1d934a59b43eebd
Téringer Gergő <71704189+Tekergo2004@users.noreply.github.com>
2024-11-23 12:23:01 +0100

    Update run.bat

--- ps/Kozpont/run.bat
+++ ps/Kozpont/run.bat
@@ -1,2 +1,2 @@
-Powershell.exe -executionpolicy remotesigned -File ".\"
-Powershell.exe -executionpolicy remotesigned -File ".\"
+Powershell.exe -executionpolicy remotesigned -File ".\ou-group-creation-kozpont.ps1"
+Powershell.exe -executionpolicy remotesigned -File ".\users-group-add-kozpont.ps1"
