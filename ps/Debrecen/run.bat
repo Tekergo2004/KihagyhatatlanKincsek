a1232d9fbcebd01cef0795bb70139a4455567d5a
Téringer Gergő <71704189+Tekergo2004@users.noreply.github.com>
2024-11-23 18:27:40 +0100

    Update run.bat

--- ps/Debrecen/run.bat
+++ ps/Debrecen/run.bat
@@ -1,2 +1,4 @@
+cd c:
+cd C:\Debrecen
 Powershell.exe -executionpolicy remotesigned -File ".\ou-group-creation-debrecen.ps1"
 Powershell.exe -executionpolicy remotesigned -File ".\users-group-add-debrecen.ps1"
