b05c0e058364634474ba2ce8a8b4d3085df7ee9f
Téringer Gergő <71704189+Tekergo2004@users.noreply.github.com>
2024-12-23 13:29:26 +0100

    Update ou-group-creation-kozpont.ps1

--- ps/Kozpont/ou-group-creation-kozpont.ps1
+++ ps/Kozpont/ou-group-creation-kozpont.ps1
@@ -1,6 +1,6 @@
 ﻿# Domain változók
 $domain = "kkk.com"
-$ouk = @(@("Vezeto", "OU=Felhasznalok,DC=kkk,DC=com"), @("IT", "OU=Felhasznalok,DC=kkk,DC=com"), @("Vezeto", "OU=Felhasznalok,DC=kkk,DC=com"), @("Kiado", "OU=Felhasznalok,DC=kkk,DC=com"), @("Adminisztracio", "OU=Felhasznalok,DC=kkk,DC=com"), @("Zene", "OU=Felhasznalok,DC=kkk,DC=com"), @("Marketing", "OU=Felhasznalok,DC=kkk,DC=com"), @("Csoportok", "OU=Felhasznalok,DC=kkk,DC=com"))
+$ouk = @(@("Felhasznalok", "DC=kkk,DC=com"),@("Vezeto", "OU=Felhasznalok,DC=kkk,DC=com"), @("IT", "OU=Felhasznalok,DC=kkk,DC=com"), @("Vezeto", "OU=Felhasznalok,DC=kkk,DC=com"), @("Kiado", "OU=Felhasznalok,DC=kkk,DC=com"), @("Adminisztracio", "OU=Felhasznalok,DC=kkk,DC=com"), @("Zene", "OU=Felhasznalok,DC=kkk,DC=com"), @("Marketing", "OU=Felhasznalok,DC=kkk,DC=com"), @("Csoportok", "OU=Felhasznalok,DC=kkk,DC=com"))
 $csoportok = @(@("Vezeto","ou=Csoportok,ou=Felhasznalok,dc=kkk,dc=com"), @("Kiado","ou=Csoportok,ou=Felhasznalok,dc=kkk,dc=com"), @("Adminisztracio","ou=Csoportok,ou=Felhasznalok,dc=kkk,dc=com"), @("Zene","ou=Csoportok,ou=Felhasznalok,dc=kkk,dc=com"), @("Marketing","ou=Csoportok,ou=Felhasznalok,dc=kkk,dc=com"), @("IT","ou=Csoportok,ou=Felhasznalok,dc=kkk,dc=com"))
 
 # OU-k létrehozása
