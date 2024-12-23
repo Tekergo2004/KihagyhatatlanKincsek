fa8bb561f4bd516bde588d5e1e1709b24a4c2a4b
Téringer Gergő <71704189+Tekergo2004@users.noreply.github.com>
2024-12-23 13:30:24 +0100

    Update ou-group-creation-debrecen.ps1

--- ps/Debrecen/ou-group-creation-debrecen.ps1
+++ ps/Debrecen/ou-group-creation-debrecen.ps1
@@ -1,6 +1,6 @@
 # Domain változók
 $domain = "debrecen.kkk.com"
-$ouk = @(@("Felhasznalok", "DC=debrecen,DC=kkk,DC=com"), @("Vezeto", "OU=Felhasznalok,DC=debrecen,DC=kkk,DC=com"), @("IT", "OU=Felhasznalok,DC=debrecen,DC=kkk,DC=com"), @("Alkalmazott", "OU=Felhasznalok,DC=debrecen,DC=kkk,DC=com"), @("Fiokvezeto", "OU=Felhasznalok,DC=debrecen,DC=kkk,DC=com"), @("Tehetseg", "OU=Felhasznalok,,DC=debrecen,DC=kkk,DC=com"), @("Kiado", "OU=Felhasznalok,DC=debrecen,DC=kkk,DC=com"), @("Csoportok", "OU=Felhasznalok,,DC=debrecen,DC=kkk,DC=com"))
+$ouk = @(@("Felhasznalok", "DC=debrecen,DC=kkk,DC=com"), @("IT", "OU=Felhasznalok,DC=debrecen,DC=kkk,DC=com"), @("Alkalmazott", "OU=Felhasznalok,DC=debrecen,DC=kkk,DC=com"), @("Fiokvezeto", "OU=Felhasznalok,DC=debrecen,DC=kkk,DC=com"), @("Tehetseg", "OU=Felhasznalok,,DC=debrecen,DC=kkk,DC=com"), @("Kiado", "OU=Felhasznalok,DC=debrecen,DC=kkk,DC=com"), @("Csoportok", "OU=Felhasznalok,,DC=debrecen,DC=kkk,DC=com"))
 $csoportok = @(@("Fiokvezeto","ou=Csoportok,ou=Felhasznalok,dc=debrecen,dc=kkk,dc=com"), @("Tehetseg","ou=Csoportok,ou=Felhasznalok,dc=debrecen,dc=kkk,dc=com"), @("Kiado","ou=Csoportok,ou=Felhasznalok,dc=debrecen,dc=kkk,dc=com"), @("IT","ou=Csoportok,ou=Felhasznalok,dc=debrecen,dc=kkk,dc=com"))
 
 # OU-k létrehozása
